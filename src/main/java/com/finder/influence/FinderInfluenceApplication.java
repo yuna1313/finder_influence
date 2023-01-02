0c60db981e7c2f756be8677eceb5e49e66e162e7
yuna <yuna38748435@gmail.com>
2023-01-02 10:19:01 +0900

    Initial commit

---
+++ src/main/java/com/finder/influence/FinderInfluenceApplication.java
@@ -0,0 +1,13 @@
+package com.finder.influence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderInfluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderInfluenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finder/influence/FinderInfluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finder.influence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderInfluenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
